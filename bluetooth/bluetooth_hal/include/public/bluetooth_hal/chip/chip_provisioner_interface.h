fa21478f959f2f2d39a37d53e72fcbbeaf5479a7
shihchienc <shihchienc@google.com>
2025-06-11 06:17:06 +0000

    chip_provisioner: Add interface doc string
    Bug: 424014348
    Test: Build passed.
    Flag: EXEMPT, no-op change.
    Change-Id: I5ea09d4f629e51f79ba3c53ee7c30c58c502cad8

--- bluetooth/bluetooth_hal/include/public/bluetooth_hal/chip/chip_provisioner_interface.h
+++ bluetooth/bluetooth_hal/include/public/bluetooth_hal/chip/chip_provisioner_interface.h
@@ -27,15 +27,51 @@ namespace chip {
 class ChipProvisionerInterface {
  public:
   using FactoryFn = std::function<std::unique_ptr<ChipProvisionerInterface>()>;
+
+  /**
+   * @brief Registers a vendor-specific factory for creating
+   * ChipProvisionerInterface instances.
+   *
+   * If a vendor factory is registered, ChipProvisionerInterface::Create() will
+   * use it. Otherwise, a default implementation will be created.
+   *
+   * @param factory The factory function to register.
+   */
   static void RegisterVendorChipProvisioner(FactoryFn factory);
 
   virtual ~ChipProvisionerInterface() = default;
 
+  /**
+   * @brief Initializes the chip provisioner.
+   *
+   * @param on_hal_state_update A callback function invoked when the HAL state
+   * changes.
+   */
   virtual void Initialize(const std::function<void(::bluetooth_hal::HalState)>
                               on_hal_state_update) = 0;
+
+  /**
+   * @brief Downloads firmware to the Bluetooth chip.
+   *
+   * @return True if firmware download is successful, false otherwise.
+   */
   virtual bool DownloadFirmware() = 0;
+
+  /**
+   * @brief Resets the firmware on the Bluetooth chip.
+   *
+   * @return True if firmware reset is successful, false otherwise.
+   */
   virtual bool ResetFirmware() = 0;
 
+  /**
+   * @brief Creates an instance of ChipProvisionerInterface.
+   *
+   * This factory method will use a registered vendor factory if available,
+   * otherwise it will create a default implementation.
+   *
+   * @return A unique_ptr to a ChipProvisionerInterface instance.
+   */
   static std::unique_ptr<ChipProvisionerInterface> Create();
 
  private:
