d79da8e706a282481b96ab8c210b2bb83dccd064
shihchienc <shihchienc@google.com>
2025-06-13 08:55:54 +0000

    config: Add firmware config tests for different transport
    Bug: 424095026
    Test: atest bluetooth_hal_firmware_config_loader_test
    Flag: EXEMPT, no-op change.
    Change-Id: I34e8ccab64c5aa9908c35e31f0fc80374ead2078

--- bluetooth/bluetooth_hal/config/firmware_config_loader_test.cc
+++ bluetooth/bluetooth_hal/config/firmware_config_loader_test.cc
@@ -17,11 +17,15 @@
 #include "bluetooth_hal/config/firmware_config_loader.h"
 
 #include <cstddef>
+#include <cstdint>
+#include <string>
+#include <string_view>
 #include <vector>
 
 #include "bluetooth_hal/config/config_constants.h"
 #include "bluetooth_hal/hal_packet.h"
 #include "bluetooth_hal/test/mock/mock_android_base_wrapper.h"
+#include "bluetooth_hal/test/mock/mock_hal_config_loader.h"
 #include "bluetooth_hal/test/mock/mock_system_call_wrapper.h"
 #include "gtest/gtest.h"
 
@@ -33,11 +37,15 @@ using ::testing::_;
 using ::testing::DoAll;
 using ::testing::Invoke;
 using ::testing::Return;
+using ::testing::ReturnRef;
+using ::testing::StrictMock;
 using ::testing::Test;
 using ::testing::Values;
 using ::testing::WithParamInterface;
 
+using ::bluetooth_hal::config::MockHalConfigLoader;
 using ::bluetooth_hal::hci::HalPacket;
+using ::bluetooth_hal::transport::TransportType;
 using ::bluetooth_hal::util::MockAndroidBaseWrapper;
 using ::bluetooth_hal::util::MockSystemCallWrapper;
 
@@ -46,24 +54,34 @@ namespace cfg_consts = ::bluetooth_hal::config::constants;
 constexpr int kLoadMiniDrvDelayMs = 100;
 constexpr int kLaunchRamDelayMs = 100;
 
-const std::string kValidContent = R"({
-  "firmware_folder_name": "/PATH/TO/FOLDER/",
-  "firmware_file_name": "FILENAME",
-  "chip_id": 123,
-  "load_mini_drv_delay_ms": 100,
-  "launch_ram_delay_ms": 100,
-  "firmware_data_loading_type": "PACKET_BY_PACKET",
-  "setup_commands": {
-    "hci_reset": [1, 3, 12, 0],
-    "hci_read_chip_id": [1, 121, 252, 0],
-    "hci_update_chip_baud_rate": [1, 24, 252, 6, 0, 0, 0, 9, 61, 0],
-    "hci_set_fast_download": [1, 114, 254, 2, 0, 1],
-    "hci_download_minidrv": [1, 46, 252, 0],
-    "hci_vsc_launch_ram": [1, 78, 252, 4, 255, 255, 255, 255],
-    "hci_read_fw_version": [1, 20, 12, 0],
-    "hci_setup_low_power_mode": [1, 39, 252, 12, 1, 24, 24, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    "hci_write_bd_address": [1, 1, 252, 6, 30, 77, 19, 43, 213, 232]
-  }
+const std::string_view kMultiTransportValidContent = R"({
+  "firmware_configs": [
+    {
+      "transport_type": 1,
+      "firmware_folder_name": "/uart/fw/",
+      "firmware_file_name": "uart_fw.bin",
+      "chip_id": 101,
+      "load_mini_drv_delay_ms": 51,
+      "launch_ram_delay_ms": 251,
+      "firmware_data_loading_type": "PACKET_BY_PACKET",
+      "setup_commands": {
+        "hci_reset": [1,3,12,0],
+        "hci_read_chip_id": [1,121,252,0]
+      }
+    },
+    {
+      "transport_type": 100,
+      "firmware_folder_name": "/vendor/fw/",
+      "firmware_file_name": "vendor_fw.bin",
+      "chip_id": 102,
+      "load_mini_drv_delay_ms": 71,
+      "launch_ram_delay_ms": 301,
+      "firmware_data_loading_type": "ACCUMULATED_BUFFER",
+      "setup_commands": {
+        "hci_update_chip_baud_rate": [1,24,252,6,0,0,0,9,61,0]
+      }
+    }
+  ]
 })";
 
 TEST(FirmwareConfigLoaderTest, IsSameSingleton) {
@@ -82,14 +100,16 @@ class FirmwareConfigLoaderTestBase : public Test {
   void SetUp() override {
     MockSystemCallWrapper::SetMockWrapper(&mock_system_call_wrapper_);
     MockAndroidBaseWrapper::SetMockWrapper(&mock_android_base_wrapper_);
+    MockHalConfigLoader::SetMockLoader(&mock_hal_config_loader_);
 
     FirmwareConfigLoader::ResetLoader();
   }
 
-  void TearDown() override {}
+  void TearDown() override { MockHalConfigLoader::SetMockLoader(nullptr); }
 
   MockSystemCallWrapper mock_system_call_wrapper_;
   MockAndroidBaseWrapper mock_android_base_wrapper_;
+  StrictMock<MockHalConfigLoader> mock_hal_config_loader_;
 };
 
 TEST_F(FirmwareConfigLoaderTestBase, GetNextFirmwareDataOnInit) {
@@ -118,12 +138,71 @@ class LoadConfigSetupCommandTypeTest
  protected:
   void SetUp() override {
     FirmwareConfigLoaderTestBase::SetUp();
-    EXPECT_TRUE(
-        FirmwareConfigLoader::GetLoader().LoadConfigFromString(kValidContent));
+
+    EXPECT_CALL(mock_hal_config_loader_, GetTransportTypePriority())
+        .WillRepeatedly(ReturnRef(empty_transport_priority_list_));
+    EXPECT_TRUE(FirmwareConfigLoader::GetLoader().LoadConfigFromString(
+        kMultiTransportValidContent));
+  }
+  const std::vector<TransportType> empty_transport_priority_list_{};
+};
+
+TEST_P(LoadConfigSetupCommandTypeTest, ParsesSetupCommandOnInit) {
+  const auto& [command_type, expected_command] = GetParam();
+  const auto command_packet =
+      FirmwareConfigLoader::GetLoader().GetSetupCommandPacket(command_type);
+
+  EXPECT_TRUE(!command_packet.has_value());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TestAllSetupCommandTypeConfig, LoadConfigSetupCommandTypeTest,
+    Values(SetupCommandValueTestParam{.command_type = SetupCommandType::kReset,
+                                      .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kReadChipId,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kUpdateChipBaudRate,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kSetFastDownload,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kDownloadMinidrv,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kLaunchRam,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kReadFwVersion,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kSetupLowPowerMode,
+               .expected_command = {}},
+           SetupCommandValueTestParam{
+               .command_type = SetupCommandType::kWriteBdAddress,
+               .expected_command = {}}));
+
+class LoadConfigSetupCommandTypeTestVendor
+    : public FirmwareConfigLoaderTestBase,
+      public WithParamInterface<SetupCommandValueTestParam> {
+ protected:
+  void SetUp() override {
+    FirmwareConfigLoaderTestBase::SetUp();
+    std::vector<TransportType> priority_list = {
+        static_cast<TransportType>(TransportType::kVendorStart)};
+    EXPECT_CALL(mock_hal_config_loader_, GetTransportTypePriority())
+        .WillRepeatedly(ReturnRef(priority_list));
+
+    EXPECT_TRUE(FirmwareConfigLoader::GetLoader().LoadConfigFromString(
+        kMultiTransportValidContent));
+    EXPECT_TRUE(FirmwareConfigLoader::GetLoader().SelectFirmwareConfiguration(
+        static_cast<TransportType>(TransportType::kVendorStart)));
   }
 };
 
-TEST_P(LoadConfigSetupCommandTypeTest, ParsesSetupCommand) {
+TEST_P(LoadConfigSetupCommandTypeTestVendor, ParsesSetupCommandForVendor) {
   const auto& [command_type, expected_command] = GetParam();
   const auto command_packet =
       FirmwareConfigLoader::GetLoader().GetSetupCommandPacket(command_type);
@@ -135,39 +214,36 @@ TEST_P(LoadConfigSetupCommandTypeTest, ParsesSetupCommand) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    TestAllSetupCommandTypeConfig, LoadConfigSetupCommandTypeTest,
+    TestVendorSetupCommandTypeConfig, LoadConfigSetupCommandTypeTestVendor,
     Values(SetupCommandValueTestParam{.command_type = SetupCommandType::kReset,
-                                      .expected_command = {0x01, 0x03, 0x0c,
-                                                           0x00}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kReadChipId,
-               .expected_command = {0x01, 0x79, 0xfc, 0x00}},
+                                      .expected_command = {}},
            SetupCommandValueTestParam{
                .command_type = SetupCommandType::kUpdateChipBaudRate,
                .expected_command = {0x01, 0x18, 0xfc, 0x06, 0x00, 0x00, 0x00,
-                                    0x09, 0x3d, 0x00}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kSetFastDownload,
-               .expected_command = {0x01, 0x72, 0xfe, 0x02, 0x00, 0x01}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kDownloadMinidrv,
-               .expected_command = {0x01, 0x2e, 0xfc, 0x00}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kLaunchRam,
-               .expected_command = {0x01, 0x4e, 0xfc, 0x04, 0xff, 0xff, 0xff,
-                                    0xff}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kReadFwVersion,
-               .expected_command = {0x01, 0x14, 0x0c, 0x00}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kSetupLowPowerMode,
-               .expected_command = {0x01, 0x27, 0xfc, 0x0c, 0x01, 0x18, 0x18,
-                                    0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00,
-                                    0x00, 0x00}},
-           SetupCommandValueTestParam{
-               .command_type = SetupCommandType::kWriteBdAddress,
-               .expected_command = {0x01, 0x01, 0xfc, 0x06, 0x1e, 0x4d, 0x13,
-                                    0x2b, 0xd5, 0xe8}}));
+                                    0x09, 0x3d, 0x00}}));
+
+TEST_F(FirmwareConfigLoaderTestBase, DumpConfigToString) {
+  std::vector<TransportType> priority_list = {TransportType::kUartH4};
+  EXPECT_CALL(mock_hal_config_loader_, GetTransportTypePriority())
+      .WillRepeatedly(ReturnRef(priority_list));
+  EXPECT_TRUE(FirmwareConfigLoader::GetLoader().LoadConfigFromString(
+      kMultiTransportValidContent));
+
+  std::string dump = FirmwareConfigLoader::GetLoader().DumpConfigToString();
+  EXPECT_NE(dump.find("Transport Type: 1"), std::string::npos);
+  EXPECT_NE(dump.find("Firmware Folder: \"/uart/fw/\""), std::string::npos);
+  EXPECT_NE(dump.find("Active Configuration for Transport Type: 1"),
+            std::string::npos);
+
+  EXPECT_NE(dump.find("Transport Type: 100"), std::string::npos);
+  EXPECT_NE(dump.find("Firmware Folder: \"/vendor/fw/\""), std::string::npos);
+
+  FirmwareConfigLoader::GetLoader().SelectFirmwareConfiguration(
+      static_cast<TransportType>(TransportType::kVendorStart));
+  dump = FirmwareConfigLoader::GetLoader().DumpConfigToString();
+  EXPECT_NE(dump.find("Active Configuration for Transport Type: 100"),
+            std::string::npos);
+}
 
 TEST(FirmwarePacketTest, HandleDifferentFirmwarePacketType) {
   std::vector<uint8_t> payload = {0x01};
