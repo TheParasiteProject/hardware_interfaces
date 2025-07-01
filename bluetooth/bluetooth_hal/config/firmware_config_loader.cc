2f1fcba459a65471ddf2da6ac12f80f567b97545
Krzysztof Kosiński <krzysio@google.com>
2025-07-01 01:35:53 +0000

    bluetooth: Avoid naming the old Status stub.
    Protobuf 22.5 replaces google::protobuf::util::Status with
    absl::Status. Use auto to avoid naming the type, making this
    code compatible with both the current and the upgraded version.
    
    Bug: 329747255
    Test: mm
    Flag: EXEMPT no runtime behavior changes
    Change-Id: I172663b900c4fcb166542952523e2596689555b9

--- bluetooth/bluetooth_hal/config/firmware_config_loader.cc
+++ bluetooth/bluetooth_hal/config/firmware_config_loader.cc
@@ -55,7 +55,6 @@ using ::bluetooth_hal::util::SystemCallWrapper;
 using ::google::protobuf::RepeatedField;
 using ::google::protobuf::util::JsonParseOptions;
 using ::google::protobuf::util::JsonStringToMessage;
-using ::google::protobuf::util::Status;
 
 namespace cfg_consts = ::bluetooth_hal::config::constants;
 
@@ -360,7 +359,7 @@ bool FirmwareConfigLoaderImpl::LoadConfigFromString(std::string_view content) {
   JsonParseOptions options;
   options.ignore_unknown_fields = true;
 
-  Status status = JsonStringToMessage(content, &container, options);
+  auto status = JsonStringToMessage(content, &container, options);
   if (!status.ok()) {
     LOG(ERROR) << __func__
                << ": Failed to parse json file, error: " << status.message();
