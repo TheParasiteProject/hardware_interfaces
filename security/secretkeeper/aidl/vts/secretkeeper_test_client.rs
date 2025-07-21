b0edf486ccaf6c6099908aaa89f989123206e677
Shikha Panwar <shikhapanwar@google.com>
2025-07-21 14:17:50 +0000

    Delete Sk Ids without creating new session
    `SkClient` constructors establish a secure session with Secretkeeper,
    which is unnecessary if store/get need not be performed. To allow robust
    cleanup of Secretkeeper IDs used in test, add an api which performs
    deletion without needing to create a session.
    
    Test: VtsSecretkeeperTargetTest
    Flag: Exempt TEST
    Bug: 421066814
    Change-Id: I12d720c4a8d3ff089829c9ef251741bdb7b947ef

--- security/secretkeeper/aidl/vts/secretkeeper_test_client.rs
+++ security/secretkeeper/aidl/vts/secretkeeper_test_client.rs
@@ -269,6 +269,13 @@ impl SkClient {
     }
 }
 
+/// Helper method to delete secrets.
+fn delete_sk_ids(instance: &str, ids: &[&Id]) {
+    let sk = get_connection(instance);
+    let ids: Vec<SecretId> = ids.iter().map(|id| SecretId { id: id.0 }).collect();
+    sk.deleteIds(&ids).unwrap();
+}
+
 #[derive(Debug)]
 enum Error {
     // Errors from Secretkeeper API errors. These are thrown by core SecretManagement and
@@ -626,11 +633,10 @@ fn secretkeeper_many_sessions_parallel(instance: String) {
     }
 
     // Remove any IDs that might have been stored in the test.
-    let sk_client = SkClient::new(&instance).unwrap();
     for idx in 0..SESSION_COUNT {
         let mut id = ID_EXAMPLE.clone();
         id.0[0] = idx as u8;
-        sk_client.delete(&[&id]);
+        delete_sk_ids(&instance, &[&id]);
     }
 
     // Now that all the parallel activity is done, should still be able to interact with
